2b0cd6b3a095a7cbaf70f1b21f48eb21c7506aba
bartosz <bartosz@c84314eb-9859-4238-a87c-7a4c937928cb>
2018-06-05 05:18:38 +0000

    Cannons

---
+++ Ship battle/Assets/Scripts/Cannon/CannonShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShoot : MonoBehaviour {
+
+    public GameObject cannonBall;
+    public float firePower;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootCannon();
+        }
+	}
+
+    public void ShootCannon()
+    {
+        GameObject thisCannonBall = Instantiate(cannonBall, transform.position, transform.rotation);
+        thisCannonBall.GetComponent<Rigidbody>().AddRelativeForce(0, 0, firePower, ForceMode.Impulse);
+    }
+}
